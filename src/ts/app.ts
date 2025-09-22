4e2def9b869091fef28d89e914476214a13988dd
Alexey96may <shulga_alexey@vk.com>
2025-09-22 19:44:51 +0300

    Fix js navigation

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,6 +1,8 @@
 import * as flsFunctions from "./modules/functions.js";
 import { Burger } from "./ui/burger.js";
+import { NavFix } from "./ui/navFixed.js";
 
 new Burger();
+new NavFix();
 
 flsFunctions.isWebp();
