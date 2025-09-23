1020ef23dcedf32f3d61d3618da56ad9bbfae45d
Alexey96may <shulga_alexey@vk.com>
2025-09-23 14:47:54 +0300

    Add modal windows in the project

--- src/js/fromTS/app.js
+++ src/js/fromTS/app.js
@@ -1,6 +1,8 @@
 import * as flsFunctions from "./modules/functions.js";
 import { Burger } from "./ui/burger.js";
 import { NavFix } from "./ui/navFixed.js";
+import { Modals } from "./ui/modals.js";
 new Burger();
 new NavFix();
+new Modals();
 flsFunctions.isWebp();
